0e36204b2c7f3eb0b2fb2b2517a0bde9a59b4e76
Christopher <christopher.caldarella@outlook.com>
2018-01-07 12:55:31 -0700

    updated Pie charts
    Pie charts for languages now use Google charts and calculate ratios on the fly
    (Now is yellow instead of blue)

--- resume_app.js
+++ resume_app.js
@@ -11,8 +11,18 @@ $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent(url) + '
     var fluency = res[6];
     fluency = fluency.replace("\"fluency_score\":","");
     fluency = Math.round(fluency*100);
+	var offset = 100-fluency;
 	
-	document.getElementById('es').innerHTML = fluency + "%";
+	//document.getElementById('es').innerHTML = fluency + "%";
+	document.getElementById('es2').innerHTML = fluency + "%";
+	
+	var spPie = "<img src ='https://chart.googleapis.com/chart?cht=p&chd=t:" + fluency + "," + offset + "&chs=100x100&chl='>";
+	//alert(imgPie);
+	document.getElementById('sp-pie').innerHTML = spPie;
+
+	var enPie = "<img src ='https://chart.googleapis.com/chart?cht=p&chd=t:100,0&chs=100x100&chl='>";
+	//alert(imgPie);
+	document.getElementById('en-pie').innerHTML = enPie;
 
 });
  
